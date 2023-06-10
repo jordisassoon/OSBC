11b5dcb1a32fb470337bb350ebf51cbe4ebcd27e
Jordan <jordisassoon@gmail.com>
2023-06-10 11:43:56 +0200

    finetuning

---
+++ finetuning.py
@@ -0,0 +1,8 @@
+from transformers import CLIPProcessor, CLIPModel
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+# save the model and processor
+model.save_pretrained("clip")
+processor.save_pretrained("clip")
