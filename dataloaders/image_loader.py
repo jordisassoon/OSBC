2490c9022eebb7f3bf49ea636212303b7fc465bd
Jordan <jordisassoon@gmail.com>
2023-06-11 19:48:03 +0200

    removed stale files

--- dataloaders/image_loader.py
+++ dataloaders/image_loader.py
@@ -4,8 +4,14 @@ from torchvision import datasets, transforms
 
 
 class ImageLoader:
-    def __init__(self, images_dir):
-        self.dataset = datasets.ImageFolder(root=images_dir)
+    def __init__(self, images_dir, image_size):
+        self.dataset = datasets.ImageFolder(root=images_dir, 
+                                            transform=transforms.Compose([
+                                                transforms.Resize(image_size),
+                                                transforms.CenterCrop(image_size),
+                                                transforms.PILToTensor()
+                                                ])
+                                            )
 
     def get_loader(self, batch_size=64, num_workers=0):
         return DataLoader(dataset=self.dataset, batch_size=batch_size, num_workers=num_workers)
